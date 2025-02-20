00ec94f6e70aac5da8bb6607db67f3655498caba
purnale <bhaktipurnale89@gmail.com>
2025-02-20 13:59:08 +0530

    add

---
+++ src/pages/Home.jsx
@@ -0,0 +1,26 @@
+import { FaLongArrowAltRight } from "react-icons/fa";
+export const Home = () => {
+
+    return (
+        <main className="hero-section main">
+           <div className="container grid grid-two-cols">
+            <div className="hero-content">
+                <h1 className="heading-xl">
+                    Explore the World, One Country at a time.
+                </h1>
+                <p className="paragraph">
+                    Discover the history, culture, and beauty of every nation. sort,
+                    search, and filter through countries to find the details you.
+                </p>
+                <button className="btn btn-darken btn-inline bg-white-box">
+                    Start Exploring <FaLongArrowAltRight />
+                </button>
+            </div>
+            <div className="hero-image">
+                <img src="/images/world.png.webp" alt="world img" 
+                className="banner-image"/>
+            </div>
+           </div>
+        </main>
+    );
+};
